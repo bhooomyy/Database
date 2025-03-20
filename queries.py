c3fda1695cfcc3ff4134a5b14d8b47b18201e566
bhooomyy <bhattbhoomi0111@gmail.com>
2025-03-20 16:45:47 -0400

    select and update query
    edge cases handled in select query. working fully efficiently.
    worked on update query. working perfectly fine.

--- queries.py
+++ queries.py
@@ -1,4 +1,5 @@
 import os,csv,pandas as pd
+import re
 current_directory=os.getcwd()
 
 def setPath(userName):
@@ -82,31 +83,137 @@ def insertQuery(query,userName):
             elif len(columns) != len(values):
                 print(f"Error: Number of columns ({len(columns)}) does not match number of values ({len(values)}).")
                 return
-    
+
+# def displayTBL(columns,tablePath):
+#     df=pd.read_csv(tablePath)
+#     csv_col_name=[col.split('.')[0] for col in df.columns]
+#     csv_col_type=[col.split('.')[1] for col in df.columns]
+#     result=[]
+#     if(len(columns)>1):
+#         for i in range(0,len(df.columns)):
+#             if columns[i]==csv_col_name[i]:
+#                 if csv_col_type[i]=='VARCHAR':
+#                     columns[i]=columns[i]
+#                 elif csv_col_type[i]=='INTEGER':
+#                     columns[i]=int(columns[i])
+#                 else:
+#                     columns[i]=float(columns[i])
+#                 result.append(columns[i])
+#     else:
+#         for i in range(0,len(csv_col_name)):
+#             if columns==csv_col_name[i]:
+#                 if csv_col_type[i]=='VARCHAR':
+#                     columns[i]=columns[i]
+#                 elif csv_col_type[i]=='INTEGER':
+#                     columns[i]=int(columns[i])
+#                 else:
+#                     columns[i]=float(columns[i])
+#                 result.append(columns[i])
+#     print(df[result])
+
 def selectQuery(query,userName):
     chunk=query.split(' ')
-    tablePath=os.path.join(setPath(userName),chunk[3]+'.csv')
-
+    tableName=query.lower().split('from')[1].strip()
+    tablePath=os.path.join(setPath(userName),tableName+'.csv')
+    
     df=pd.read_csv(tablePath)
     columns=[col.split('.')[0] for col in df.columns]
     df.columns=columns
+   
     if(chunk[1]=='*'):
         print(df)
+        return
+    
+    select_part = query.split('select')[1].split('from')[0].strip()
+    isValid=True
+    result=[]
+    if ',' in select_part:
+        cols = [col.strip() for col in select_part.split(',')]
     else:
-        isValid=True
-        result=[]
-        column=chunk[1].split(',')
-        for col in column:
-            if col in df.columns:
-                result.append(col)
-            else:
-                isValid=False
-                print(f'No such column name "{col}" exists')
+        cols = [select_part.strip()]
+    
+    for col in cols:
+        if col in df.columns:
+            result.append(col)
+        else:
+            isValid = False
+            print(f'Error: Column "{col}" does not exist in the table.')
+    
+    if isValid:
+        print(df[result])
+    else:
+        print('Error: Invalid columns in the query.')
 
-        if isValid:
-            print(df[result])
+def updateQuery(query,userName):
+    chunk=query.split(' ')
+    tablepath=os.path.join(setPath(userName),chunk[1].strip()+'.csv')
+    parts = re.split(r'\bset\b|\bwhere\b', query, flags=re.IGNORECASE)
+    requestedCol = parts[1].strip()
+    requestedCol=requestedCol.split(',')
+    conditions = parts[2].strip() 
+    #many conditions connected by AND OR , Asuume we have one condition here
+    req_col=[req_col.split('=')[0] for req_col in requestedCol]
+    req_val=[req_val.split('=')[1] for req_val in requestedCol]
+
+    condition_name=conditions.split('=')[0]
+    condition_val=conditions.split('=')[1]
+    
+    df=pd.read_csv(tablepath,na_values=['NULL', 'NONE'])
+    csv_col_names={col.split('.')[0]:col for col in df.columns}
+    #csv_col_type=[col.split('.')[1] for col in df.columns]
+    
+    fetch_col=[]
+    fetch_col=[condition_name]+req_col
+   
+    isValid=True
+    for col in fetch_col:
+        if col not in csv_col_names:
+            print(f'Error {col} doesn\'t exist in table')
+            isValid=False
+    
+    if isValid:
+        condition_type_check_full=csv_col_names[condition_name]
+        condition_type_check=condition_type_check_full.split('.')[1]
+        if condition_type_check=='INTEGER':
+            condition_val=int(condition_val)
+        elif condition_type_check=='VARCHAR':
+            condition_val=condition_val
+        elif condition_type_check=='FLOAT' or condition_type_check=='DOUBLE':
+            condition_val=float(condition_val)
+
+        # isNaN=False
+        # for row in df.iterrows():
+        #     if pd.isna(str(row[condition_type_check_full])) and pd.isna(condition_val):
+        #         isNaN=True
+
+        for index,row in df.iterrows():
+            if row[condition_type_check_full]==condition_val:
+                for j,col in enumerate(req_col):
+                    full_col=csv_col_names[col]
+                    col_type=full_col.split('.')[1]
+
+                    # if isNaN:
+                    if pd.isna(row[full_col]):
+                        if col_type=='INTEGER':
+                            df.at[index,full_col]=0
+                        elif col_type=='VARCHAR':
+                            df.at[index,full_col]="'"
+                        elif col_type in ['FLOAT','DOUBLE']:
+                            df.at[index,full_col]=0.0
+
+                    if col_type=='INTEGER':
+                        req_val[j]=int(req_val[j])
+                    elif col_type=='VARCHAR':
+                        req_val[j]=req_val[j]
+                    elif col_type in ['FLOAT','DOUBLE']:
+                        req_val[j]=float(req_val[j])
+                    
+                    df.at[index,full_col]=req_val[j]
         
-            
+        df.to_csv(tablepath, index=False)
+        print(f'Success: Table "{chunk[1]}" updated.')
+    else:
+        print('Error: Data not updated due to validation errors.')
 
 def typeOfQuery(query, userName):
     chunk = query.split(' ')
@@ -128,9 +235,13 @@ def typeOfQuery(query, userName):
             return
         insertQuery(query, userName)
     elif command == 'select':
-        if chunk[2].lower() != 'from':
+        if 'from' not in query.lower():
             print('Error: Invalid query format. Expected "SELECT <columns_name> FROM <table_name>".')
             return
         selectQuery(query,userName)
+    elif command=='update':
+        if 'set' and 'where' not in query.lower():
+            print('Error: Invalid query format. Expected "UPDATE <table_name> SET <column1_name> = <value1>,... WHERE <condition>".')
+        updateQuery(query,userName)
     else:
         print('Error: Unsupported query type.')
